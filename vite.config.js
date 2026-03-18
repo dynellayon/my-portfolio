18902120cc1231e2ff90c31718b48f7011cdf2c7
dynellayon <danayon3@gmail.com>
2026-03-18 15:15:19 +0800

    new changes

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-portfolio/", // 👈 VERY IMPORTANT
   // Now you can use __dirname
   resolve: {
     alias: {
